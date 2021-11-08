fbdc27cb2cf9fd9d18ce5af43aaa9915da85714a
pawanesh mishra <49597893+pawanesh5@users.noreply.github.com>
2021-11-08 23:54:54 +0530

    Add files via upload

---
+++ develop-ui-with-theme-changer/js/script.js
@@ -0,0 +1,11 @@
+function changeTheme(event){
+    if(document.getElementById("bright").checked){
+        document.body.style.background = 'white';
+    }
+    if(document.getElementById("cool").checked){
+        document.body.style.background = 'rgb(237, 192, 171)';
+    }
+    if(document.getElementById("fiery").checked){
+        document.body.style.background = 'rgb(255, 112, 3)';
+    }
+    }
