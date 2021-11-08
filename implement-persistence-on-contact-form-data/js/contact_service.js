15d6da2d03549298823f5a249d4d1c5e746b1028
pawanesh mishra <49597893+pawanesh5@users.noreply.github.com>
2021-11-08 23:55:32 +0530

    Add files via upload

---
+++ implement-persistence-on-contact-form-data/js/contact_service.js
@@ -0,0 +1,13 @@
+// Provide persistence solution code here
+
+function sendData(data) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 201) {
+        console.log("succefully added");
+      }
+    };
+    xhr.open("POST", "http://localhost:3000/contact", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   xhr.send(JSON.stringify(data));
+  }
